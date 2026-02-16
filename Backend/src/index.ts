23529d1e6edd81f264eb43c6e8dfd20a0e67b71f
PDGamerSG <pallab4586das@gmail.com>
2026-02-17 03:06:17 +0530

    Backend code added

---
+++ Backend/src/index.ts
@@ -0,0 +1,37 @@
+ import { WebSocketServer,WebSocket } from "ws";
+ const wss = new WebSocketServer({port :8080})
+
+ interface User{
+    socket: WebSocket;
+    room:string;
+ }
+ let allSockets: User[] = [];
+
+ wss.on("connection" , (socket)=> {
+    socket.on("message",(message)=>{
+        // @ts-ignore
+        const parsedMessage = JSON.parse(message as unknown as string);
+        if(parsedMessage.type === "join"){ // based on the schema
+            console.log("user joined the room "+parsedMessage.payload.roomId);
+            allSockets.push({
+                socket,
+                room:parsedMessage.payload.roomId
+            })
+        }
+        if(parsedMessage.type == "chat"){
+            console.log("user wants to chat");
+            let currentUserRoom = null;
+            for(let i=0;i<allSockets.length;i++){
+                if(allSockets[i]?.socket == socket){
+                    currentUserRoom = allSockets[i]?.room
+                }
+            }
+            for(let i=0;i<allSockets.length;i++){
+                if(allSockets[i]?.room == currentUserRoom){
+                    allSockets[i]?.socket.send(parsedMessage.payload.message)
+                }
+            }
+
+        }
+    })
+ })
